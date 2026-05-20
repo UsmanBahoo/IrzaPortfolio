0e87d6e0eeefd97a264c2ccf0558dec662ec48f8
Usman Bahoo <usmanbahoo381@gmail.com>
2026-05-20 07:09:17 +0500

    fix: Adjust gap in Tools component for improved layout

--- src/page/Tools.jsx
+++ src/page/Tools.jsx
@@ -7,7 +7,7 @@ function Tools() {
             <h2 className="text-3xl font-bold tracking-tight sm:text-4xl text-center">  
             Tools I Use
             </h2>
-        <div className="mt-8 flex flex-wrap gap-20 justify-center ">
+        <div className="mt-8 flex flex-wrap gap-10 justify-center ">
           <img src="/assets/vscode.png" alt="VS Code" className="w-20 h-20" />
           <img src="/assets/ChatGPT.png" alt="ChatGPT" className="w-20 h-20" />
           <img src="/assets/chrome.png" alt="Chrome" className="w-20 h-20" />
