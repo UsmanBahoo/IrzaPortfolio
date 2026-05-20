d592c84b5bca2dc0442670b5dc7b1dc4e8a21db8
Usman Bahoo <usmanbahoo381@gmail.com>
2026-05-20 07:24:48 +0500

    fix: Update link in Banner component to point to the correct PDF file

--- src/page/Banner.jsx
+++ src/page/Banner.jsx
@@ -75,7 +75,7 @@ function Banner() {
                 <div className="square"></div>
               </span>
               <p className={`title resume-title text-white`}>Resume</p>
-              <a href='' className="title">Open</a>
+              <a href='/src/assets/IrzaRaman.pdf' className="title">Open</a>
             </label>
           </div>
         </div>
