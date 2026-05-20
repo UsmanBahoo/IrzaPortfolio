4b393a1f18df25718387ab17d1b121321d1737ba
Usman Bahoo <usmanbahoo381@gmail.com>
2026-05-20 09:16:06 +0500

    feat: Add Project section and remove Skillsdetail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
 import Navbar from './component/Navbar'
 import About from './page/About'
 import Banner from './page/Banner'
 import Card from './page/Card'
-import Projectdetail from './page/Skillscard'
 import Skills from './page/Skills'
 import Tools from './page/Tools'
 import Skillscard from './page/Skillscard'
+import Project from './page/Project'
 import Testimonials from './page/Testimonials'
 import Contact from './page/Contact'
+
 function App() {
   return (
     <div>
@@ -19,6 +19,7 @@ function App() {
       <Skills />
       <Tools />
       <Skillscard />
+      <Project />
       <Testimonials />
       <Contact />
     </div>
